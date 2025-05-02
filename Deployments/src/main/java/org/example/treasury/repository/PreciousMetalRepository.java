08101d502a9da90939c361f9bcb5bc852e51b068
BiesterVictor <victor.biester@nordlb.de>
2025-05-02 10:13:02 +0200

    chore: clean

--- Deployments/src/main/java/org/example/treasury/repository/PreciousMetalRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.treasury.repository;
-
-import org.example.treasury.model.PreciousMetal;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PreciousMetalRepository extends MongoRepository<PreciousMetal, String> {
-}
