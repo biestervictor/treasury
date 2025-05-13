2754dd413c485a9d4b415c60fa46eecaaf5b7f2d
BiesterVictor <victor.biester@nordlb.de>
2025-05-13 14:30:56 +0200

    fix: add TestConfig

---
+++ src/test/java/org/example/treasury/TestConfig.java
@@ -0,0 +1,26 @@
+package org.example.treasury;
+
+import org.example.treasury.repository.DisplayRepository;
+import org.example.treasury.repository.PreciousMetalRepository;
+import org.example.treasury.repository.ShoeRepository;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+
+    @Bean
+    public ShoeRepository shoeRepository() {
+        return Mockito.mock(ShoeRepository.class);
+    }
+    @Bean
+    public DisplayRepository displayRepository() {
+        return Mockito.mock(DisplayRepository.class);
+    }
+    @Bean
+    public PreciousMetalRepository preciousMetalRepository() {
+        return Mockito.mock(PreciousMetalRepository.class);
+    }
+
+}
